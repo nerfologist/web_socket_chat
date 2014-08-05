6eb694434adfc8a96c04036959b072b678229c6b
Marco Arduini <mar.arduini@gmail.com>
2014-08-05 10:09:20 -0700

    committing existing work

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,10 +2,25 @@
   var ChatApp = root.ChatApp = (root.ChatApp || {});
   
   var Chat = ChatApp.Chat = function (socket) {
+    var that = this;
     this.socket = socket;
+    this.room = null;
+    
+    socket.on('room_change_result', function (data) {
+      console.log('changing this.room');
+      that.room = data.room;
+    })
   };
   
   Chat.prototype.sendMessage = function (message) {
-    this.socket.emit('from_browser_event', { message: message });
+    this.socket.emit('from_browser_event', { message: message, room: this.room });
+  };
+  
+  Chat.prototype.requestNicknameChange = function (nickname) {
+    this.socket.emit('nickname_change_request', { nickname: nickname });
+  };
+  
+  Chat.prototype.joinRoom = function (roomName) {
+    this.socket.emit('room_change_request', { room: roomName });
   };
 })(this);
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -2,19 +2,17 @@ $(document).ready(function () {
   var socket = io.connect();
   var chat = new ChatApp.Chat(socket);
   
-  var parseForm = function () {
+  var parseMsgForm = function () {
     return $('#msg_input').val();
   };
+  
+  // var parseNicknameForm = function () {
+  //   return $('#nickname_input').val();
+  // };
   
   var sendMessage = function (msg) {
     chat.sendMessage(msg);
   };
-  
-  $('form').on('submit', function (event) {
-    event.preventDefault();
-    var msg = parseForm();
-    sendMessage(msg);
-  });
   
   var appendMsg = function (msg) {
     var $li = $('<li>');
@@ -22,7 +20,52 @@ $(document).ready(function () {
     $('#msg_target').append($li);
   };
   
+  var sendNicknameChangeRequest = function (newNick) {
+    chat.requestNicknameChange(newNick);
+  };
+  
+  var sendRoomChangeRequest = function (newRoom) {
+    chat.joinRoom(newRoom);
+  };
+  
+  $('form#messageForm').on('submit', function (event) {
+    event.preventDefault();
+    var msg = parseMsgForm();
+    
+    // custom commands
+    if (msg.match(/^\/.*/)) {
+      return _handleCommand(msg);
+    }
+    
+    sendMessage(msg);
+  });
+  
+  var _handleCommand = function (cmd) {
+    // nickname change
+    if (cmd.match(/^\/nick/)) {
+      var nick = cmd.match(/\/nick\s*(\w+)/)[1];
+      return sendNicknameChangeRequest(nick);
+    } else if (cmd.match(/^\/room/)) {
+      var room = cmd.match(/\/room\s*(\w+)/)[1];
+      return sendRoomChangeRequest(room);
+    }
+  };
+
+  // $('form#nicknameForm').on('submit', function (event) {
+  //   event.preventDefault();
+  //   var newNick = parseNicknameForm();
+  //   sendNicknameChangeRequest(newNick);
+  // });
+  
   socket.on('from_node_event', function (data) {
     appendMsg(data.message);
   });
+  
+  socket.on('nickname_change_result', function (data) {
+    appendMsg('nickname changed to ' + data.nickname);
+  });
+  
+  socket.on('room_change_result', function (data) {
+    console.log(data.message);
+  })
 });
